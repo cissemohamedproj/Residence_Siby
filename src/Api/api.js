9e03c0d9712b12182733d46722ef963947c22bbf
McOriginal <originalmc223@gmail.com>
2026-04-26 06:44:23 +0000

    Changement de URL backend

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backend-residence-siby.onrender.com/api',
+  baseURL: 'https://backend-residence-siby-0dg3.onrender.com/api',
   // baseURL: 'http://localhost:5000/api',
 
   headers: {
